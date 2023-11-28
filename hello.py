5fb7ca470cc2e50be783c211bb6d9fc5fc0a00e9
Deepa-Mathad <67700214+Deepa-Mathad@users.noreply.github.com>
2023-11-28 15:31:05 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,13 +2,16 @@
 import json
 
 def initial():
-  print('Hello World')
-  # some JSON:
-  x = '{ "name":"John", "age":30, "city":"New York"}'
-  
-  # parse x:
-  y = json.loads(x)
-  # return x
-  
-  # the result is a Python dictionary:
-  print(y["number"])
+  try:
+    print('Hello World')
+    # some JSON:
+    x = '{ "name":"John", "age":30, "city":"New York"}'
+    
+    # parse x:
+    y = json.loads(x)
+    # return x
+    
+    # the result is a Python dictionary:
+    print(y["number"])
+  except Exception as e:
+    print("error", e)
