bdd8e61765f1408b1a842a31487d723db804eced
Deepa-Mathad <67700214+Deepa-Mathad@users.noreply.github.com>
2023-12-08 12:17:35 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,21 +3,15 @@ import json
 import sys
 
 def initial():
-    try:
-        # some JSON:
-        x = '{ "name":"John", "age":30, "city":"New York"}'
-        
-        # parse x:
-        y = json.loads(x)
-        # return x
-        
-        # the result is a Python dictionary:
-        print(y["name"])
-        print(y["number"])
-        print(y["age"])
-        print("End of function initial")
-    except Exception as e:
-        print("initial function exception", e)
-        print(f"Exception: {e}")
-        # Exit with a non-zero status code
-        sys.exit(1)
+    # some JSON:
+    x = '{ "name":"John", "age":30, "city":"New York"}'
+    
+    # parse x:
+    y = json.loads(x)
+    # return x
+    
+    # the result is a Python dictionary:
+    print(y["name"])
+    print(y["number"])
+    print(y["age"])
+    print("End of function initial")
